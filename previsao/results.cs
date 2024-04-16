dad3cfec95f0ddb0d495f93cf32714a178c78679
previsao <kauemroberto@gmail.com>
2024-04-16 11:52:20 -0300

    isssomemo

---
+++ previsao/results.cs
@@ -0,0 +1,31 @@
+namespace previsao;
+
+public class results
+{
+     public int temp {get; set;}
+
+   public string description {get; set;}
+
+   public string condition_code {get; set;} 
+
+   public string currently {get; set;}
+
+   public string img_id {get; set;}
+
+   public string city {get; set;}
+
+   public string humidity {get; set;}
+
+   public string rain {get; set;}
+
+   public string sunrise {get; set;}
+
+   public string sunset {get; set;}
+
+   public string wind_speedy {get; set;}
+
+   public string wind_direction {get; set;}
+
+   public string moon_phase {get; set;}
+
+}
